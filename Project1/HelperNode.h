6bd2030437bfbe6df4a3e74afc717db9a8f5f4ca
Justin Kennard <65385473+keyboard-jo@users.noreply.github.com>
2023-05-17 21:48:38 +0800

    add compare method to university and mergeSort should work

--- Project1/HelperNode.h
+++ Project1/HelperNode.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <string>
 
+// Just to make the intellisense work for template
+
 class HelperNode
 {
 public:
-	HelperNode* nextAdd;
-	HelperNode* prevAdd;
+	bool compareAttributes(HelperNode * otherNode, string attribute);
 	string getValueOf(string attributes) {
 	}
 	HelperNode() {
