7a1d4a1bfffd626ae5180ffb6fb43f7d85d2c61e
1willliam <114719992+1willliam@users.noreply.github.com>
2023-05-26 16:06:54 +0800

    update linearsearch in university class

--- Project1/LinearSearch.h
+++ Project1/LinearSearch.h
@@ -5,13 +5,32 @@
 using namespace std;
 
 template <class T>
-T* linearSearch(T* head, string data) {
+T* linearSearch(T* head, string data, int opt) {
 	T* node = head;
-	while (node != NULL) {
-		if (stoi(data) != node->rank) {
-			return node;
+	if (opt == 1){
+		while (node != NULL) {
+			if (stoi(data) == node->rank) {
+				return node;
+			}
+			node = node->nextAdd;
 		}
-		node = node->nextAdd;
 	}
+	else if (opt == 2){
+		while (node != NULL) {
+			if (data == node->institution) {
+				return node;
+			}
+			node = node->nextAdd;
+		}
+	}
+	//else if (opt == "3"){
+	//	while (node != NULL) {
+	//		if (data == node->user) {
+	//			return node;
+	//		}
+	//		node = node->nextAdd;
+	//	}
+	//}
+
 	return nullptr;
 }
