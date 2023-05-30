77746d8c64268f8b522a95e4d1b4176b89db4d24
JinnOppa <eugene.winata@gmail.com>
2023-05-30 12:54:57 +0800

    Removing unused code and provide explanation of the binary search algorithm and implementations

--- Project1/BinarySearch.h
+++ Project1/BinarySearch.h
@@ -3,25 +3,30 @@
 
 using namespace std;
 
-
-
 template <class T>
 T* middle(T* start, T* end) {
+	// Check if the start pointer is NULL
 	if (start == NULL) {
 		return NULL;
 	}
 	T* slow = start;
 	T* fast = start->nextAdd;
 	
-	while (fast != end /*&& fast != NULL*/)
+	// Use two pointers to find the middle element in a linked list
+	// The fast pointer moves twice as fast as the slow pointer
+	// When the fast pointer reaches the end or becomes NULL, the slow pointer will be at the middle
+
+	while (fast != end )
 	{
 		fast = fast->nextAdd;
+		// Move the slow pointer only if the fast pointer is not at the end or NULL
 		if (fast != end && fast != NULL) {
 			slow = slow->nextAdd;
 			fast = fast->nextAdd;
 		}
 	}
-	
+
+	// Return the middle element
 	return slow;
 }
 
@@ -29,60 +34,44 @@ template <class T>
 T* binarySearch(T* head, string data, string input) {
 	T* start = head;
 	T* end = NULL;
-	//finding middle node
+
+	// Perform binary search based on the input parameter
 	if (input == "rank") {
+		// Search by rank
 		while (start != end) {
 			T* midNode = middle(start, end);
-			if (stoi(data) == midNode->rank/* || data == midNode->institution || data == midNode->Location*/) {
+			// Check if the rank of the middle node matches the search data
+			if (stoi(data) == midNode->rank) {
 				return midNode;
 			}
-
-
-			else if (midNode->rank > stoi(data)/* || midNode->institution > data || midNode->Location > data*/) {
-				end = midNode/*->prevAdd*/;
+			else if (midNode->rank > stoi(data)) {
+				// If the rank of the middle node is greater, search in the first half
+				end = midNode;
 			}
-
 			else {
+				// If the rank of the middle node is smaller, search in the second half
 				start = midNode->nextAdd;
 			}
 		}
 	}
 	else if (input == "institution") {
+		// Search by institution name
 		while (start != end) {
 			T* midNode = middle(start, end);
-			if (data.compare(midNode->institution) == 0/* || data == midNode->institution || data == midNode->Location*/) {
+			// Compare the institution name of the middle node with the search data
+			if (data.compare(midNode->institution) == 0) {
 				return midNode;
 			}
-
-
-			else if (midNode->institution.compare(data) > 0/* || midNode->institution > data || midNode->Location > data*/) {
-				end = midNode/*->prevAdd*/;
+			else if (midNode->institution.compare(data) > 0) {
+				// If the institution name of the middle node is greater, search in the first half
+				end = midNode;
 			}
-
 			else {
+				// If the institution name of the middle node is smaller, search in the second half
 				start = midNode->nextAdd;
 			}
 		}
 	}
-	
-	//while (start != end)
-	//{
-	//	T* midNode = middle(start, end);
-	//	string objectData = midNode->getValueOf(input);
-	//	/*cout << objectData << endl;*/
-	//	if (data == objectData)
-	//	{
-	//		return midNode;
-	//	}
-	//	else if (objectData > data) {
-	//		end = midNode->prevAdd;
-	//	}
-
-	//	else {
-	//		start = midNode->nextAdd;
-	//	}
-	//}
+	// Return NULL if the search data is not found
 	return NULL;
-
-	
 }
